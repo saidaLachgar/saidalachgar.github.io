c6fc892a0a3e178ad42ec487ddf8459973c1b45f
saidaLachgar <saidalachgar.div@gmail.com>
2021-01-05 05:11:40 +0100

    kee

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 function resize(){
-    var device = $(window).height(),
-        height= $("#nav").height() + $("#interface").height();
-
-    if(height < device){
-        $("#container").height("100vh");
-        $("#interface").height("100vh");
-        document.getElementById("interface").style.marginBottom = "10px";
-    }else{
-        $("#container").height("unset");
-        $("#interface").height("unset");
-        document.getElementById("interface").style.marginBottom = "100px";
-    }
+    var nav= $("#nav").height();
+    document.getElementById("container").style.marginTop = nav*(-1)+"px";
 }
 resize();
 $(window).on('resize', function(){
@@ -48,27 +38,26 @@ $(document).ready(function(){
 });
 
 function scrollDown(ElementId){
-        document.getElementById(ElementId).scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
-    }
-    
-    const scrollToTopButton = document.getElementById('js-top');
-    const scrollFunc = () => {
+    document.getElementById(ElementId).scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
+}
+const scrollToTopButton = document.getElementById('js-top');
+const scrollFunc = () => {
     let y = window.scrollY;
     if (y > 0) {
         scrollToTopButton.className = "top-link show";
     } else {
         scrollToTopButton.className = "top-link hide";
     }
-    };
-    window.addEventListener("scroll", scrollFunc);
-    const scrollToTop = () => {
+};
+window.addEventListener("scroll", scrollFunc);
+const scrollToTop = () => {
     const c = document.documentElement.scrollTop || document.body.scrollTop;
     if (c > 0) {
         window.requestAnimationFrame(scrollToTop);
         window.scrollTo(0, 0);
     }
-    };
-    scrollToTopButton.onclick = function(e) {
+};
+scrollToTopButton.onclick = function(e) {
     e.preventDefault();
     scrollToTop();
-    }
+}

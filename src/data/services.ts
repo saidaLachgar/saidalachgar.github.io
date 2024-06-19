4bdcc9e8a3fe551e0dda585d78ed5ece02687341
Saida-lachgar <s.lachgar@picaxel.com>
2024-06-19 23:22:19 +0100

    update work content

--- src/data/services.ts
+++ src/data/services.ts
@@ -14,45 +14,45 @@ const services: Service[] = [
     title: "Custom E-commerce Theme Styling",
     description: "Craft visually stunning and unique themes for e-commerce platforms such as PrestaShop and Sylius. Specializing in frontend development to enhance the visual appeal and user interaction of online storefronts.",
     icon: {
-      name: "circles-around-star",
-      width: 240,
-      height: 205
+      name: "e-commerce",
+      width: 100,
+      height: 100
     },
   },
   {
     title: "SEO Core Web Vitals Optimization",
     description: "Prioritize the improvement of Core Web Vitals metrics for better website performance and improved search engine rankings.",
     icon: {
-      name: "web",
-      width: 242,
-      height: 242
+      name: "seo",
+      width: 102,
+      height: 102
     },
   },
   {
     title: "Responsive Design and Cross-Browser Compatibility",
     description: "Ensure seamless adaptation to different screen sizes and flawless functionality across various browsers, creating a consistent user experience through responsive design.",
     icon: {
-      name: "circles-as-rectangle",
-      width: 242,
-      height: 150
+      name: "responsive",
+      width: 101,
+      height: 100
     },
   },
   {
     title: "Code Review, Refactoring & Migration",
     description: "Elevate projects through precise code reviews, strategic refactoring, seamless stack migrations, and developer guidance, ensuring top-tier code quality and cohesive frontend implementation.",
     icon: {
-      name: "circles-as-cylinder",
-      width: 270,
-      height: 234
+      name: "refactoring",
+      width: 100,
+      height: 100
     },
   },
   {
     title: "Interactive Website Motions",
     description: "Enhance websites with captivating motions using cutting-edge technologies like Three.js and GSAP, incorporating dynamic and interactive elements to boost user engagement.",
     icon: {
-      name: "circles-as-sphere",
-      width: 242,
-      height: 270
+      name: "motion",
+      width: 103,
+      height: 100
     },
   },
 

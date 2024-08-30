e25ab1fb4601b3fddc998e31fbfcc7a3a1cc5ee6
Saida-lachgar <s.lachgar@picaxel.com>
2024-08-30 22:08:44 +0100

    project list

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
-import { defineConfig } from 'astro/config';
-import { SITE_URL } from "./src/data/config";
-import astroMetaTags from "astro-meta-tags"
-import netlify from '@astrojs/netlify';
-import sitemap from '@astrojs/sitemap';
-import compress from 'astro-compress';
-import icon from 'astro-icon';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config'
+import { SITE_URL } from './src/data/config'
+import astroMetaTags from 'astro-meta-tags'
+import netlify from '@astrojs/netlify'
+import sitemap from '@astrojs/sitemap'
+import compress from 'astro-compress'
+import icon from 'astro-icon'
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
+  prefetch: true,
   compressHTML: true,
   integrations: [
     mdx(),
@@ -17,7 +18,7 @@ export default defineConfig({
     astroMetaTags(),
     sitemap(),
     icon({
-      iconDir: "src/assets/icons",
+      iconDir: 'src/assets/icons',
     }),
   ],
   output: 'server',

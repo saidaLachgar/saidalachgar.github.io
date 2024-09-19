6af75b612201362e0a5980400a4d5ab655e0a081
Saida-lachgar <s.lachgar@picaxel.com>
2024-09-20 00:39:57 +0100

    blog content

--- src/consts.ts
+++ src/consts.ts
@@ -109,7 +109,6 @@ export const NAVIGATION = [
 
 export const POST_METADATA = {
   defaultLayout: "column", // Default layout for blog posts, options: simple and column
-  showFullWidthCover: false, // Show full width cover image in blog post
   showCover: true, // Show cover image in blog post
   showTags: true, // Show tags in blog post, TODO: Add support for hiding tags
   showDate: true, // Show date in blog post, TODO: Add support for hiding date
@@ -117,5 +116,4 @@ export const POST_METADATA = {
   showAuthors: true, // Show authors in blog post, TODO: Add support for hiding authors
   showRelatedPosts: true, // Show related posts in blog post, TODO: Add support for hiding related posts
   showTableOfContents: true, // Show table of contents in blog post
-  showShareButtons: 'both', // Show share buttons in blog post, options: top, bottom, both, none
 };

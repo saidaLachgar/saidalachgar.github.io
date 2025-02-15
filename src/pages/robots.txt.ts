9c5ef12b3c301758e1bbd66000fa023985ab1693
Saida-lachgar <s.lachgar@picaxel.com>
2025-02-15 03:17:52 +0100

    privacy policy page

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,13 +1,15 @@
 import type { APIRoute } from 'astro';
 
-const getRobotsTxt = (sitemapURL: URL) => `
+const getRobotsTxt = (sitemapURL: URL, policiesURL: URL) => `
 User-agent: *
 Allow: /
 
 Sitemap: ${sitemapURL.href}
+Policies: ${sitemapURL.href}
 `;
 
 export const GET: APIRoute = ({ site }) => {
     const sitemapURL = new URL('sitemap-index.xml', site);
-    return new Response(getRobotsTxt(sitemapURL));
+    const policiesURL = new URL('privacy-policy', site);
+    return new Response(getRobotsTxt(sitemapURL, policiesURL));
 };

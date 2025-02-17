ae349a92bef944ecb788a52e5a37e3c22ce48caa
Saida-lachgar <s.lachgar@picaxel.com>
2025-02-18 00:56:14 +0100

    add astro-font + exclude privacy-policy and blog pagination from sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import mdx from '@astrojs/mdx'
-// import netlify from '@astrojs/netlify'
 import sitemap from '@astrojs/sitemap'
 import compress from 'astro-compress'
 import icon from 'astro-icon'
-import astroMetaTags from 'astro-meta-tags'
+import purgecss from 'astro-purgecss'
 import metaTags from 'astro-meta-tags'
 import robotsTxt from 'astro-robots-txt'
 import { defineConfig } from 'astro/config'
@@ -17,12 +16,28 @@ export default defineConfig({
   integrations: [
     mdx(),
     compress(),
-    astroMetaTags(),
-    sitemap(),
+    purgecss({
+      fontFace: true, // removes any unused @font-face if set to true
+      rejected: true, // scan through the removed list to see if there's anything wrong
+      rejectedCss: true, // keeps the discarded CSS
+      variables: true, // removes any unused CSS variables if set to true
+      keyframes: false, // removes unused keyframes by setting if set to true
+      safelist: {
+        greedy: [
+          /*astro*/
+        ],
+      },
+    }),
+    sitemap({
+      filter: (page) => page !== `${SITE_METADATA.siteUrl}/privacy-policy/` && !/\/blog\/\d+\/$/.test(page),
+    }),
     metaTags(),
     robotsTxt(),
     icon({
       iconDir: 'src/assets/icons',
     }),
   ],
+  build: {
+    inlineStylesheets: 'never',
+  },
 })
